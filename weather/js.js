d60978cf6b546cdae7bef62ce0249e18878cfb87
Mashawabupr <marikanik1999@gmail.com>
2022-06-18 13:13:33 +0300

    add forecast(fake data)

--- weather/js.js
+++ weather/js.js
@@ -1,81 +1,79 @@
 //CURRENT DATE
-function cuurentDate(){
-    let nowDate=new Date()
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Satuday"
-      ];
-      
-      let day=nowDate.getDay()
-        let hours=nowDate.getHours()
-        let minutes=nowDate.getMinutes()
-        if (minutes<10){
-            minutes=`0${minutes}`
-            }
-        if (hours<10){
-                hours=`0${hours}`
-            
-        }
-        return `${days[day]} ${hours}:${minutes}`
-
-} document.querySelector("#currentDate").innerHTML=cuurentDate()
 
 //SEARCHING CITY
 
-function showWhatCity(input){
-    let apiKey="0a438755c0214c93834115811221606"
-    let apiURL=`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input}`
-    axios.get(apiURL).then(showApi)
+function showWhatCity(input) {
+  let apiKey = "0a438755c0214c93834115811221606";
+  let apiURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input}`;
+  axios.get(apiURL).then(showApi);
 }
-showWhatCity("Paris")
-function showApi(response){
-    //CITY
-    
-    document.querySelector("#gradus").innerHTML=Math.round(response.data.current.temp_c)
-    
-        document.querySelector("#showCity").innerHTML=response.data.location.name
+showWhatCity("Paris");
+function showApi(response) {
+  //CITY
+  console.log(response.data.location.localtime.split(" ")[1]);
+  function cuurentDate() {
+    let nowDate = new Date();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Satuday",
+    ];
 
-        function cels(){
-            document.querySelector("#gradus").innerHTML=Math.round(response.data.current.temp_c)
-        }document.querySelector("#cels").addEventListener("click",cels)
+    let day = nowDate.getDay();
+    let time = response.data.location.localtime.split(" ")[1];
+    return `${days[day]} ${time}`;
+  }
+  document.querySelector("#currentDate").innerHTML = cuurentDate();
+  document
+    .querySelector("#img")
+    .setAttribute("href", response.data.current.condition.icon);
+  document.querySelector("#gradus").innerHTML = Math.round(
+    response.data.current.temp_c
+  );
 
-        function faren(){
-            document.querySelector("#gradus").innerHTML=Math.round(response.data.current.temp_f)
-        }
-        document.querySelector("#faren").addEventListener("click",faren)
-    document.querySelector("#description").innerHTML=response.data.current.condition.text
+  document.querySelector("#showCity").innerHTML = response.data.location.name;
+
+  function cels() {
+    document.querySelector("#gradus").innerHTML = Math.round(
+      response.data.current.temp_c
+    );
+  }
+  document.querySelector("#cels").addEventListener("click", cels);
+
+  function faren() {
+    document.querySelector("#gradus").innerHTML = Math.round(
+      response.data.current.temp_f
+    );
+  }
+  document.querySelector("#faren").addEventListener("click", faren);
+  document.querySelector("#description").innerHTML =
+    response.data.current.condition.text;
 }
 
-function showCity(event){
-    event.preventDefault()
-    let input=document.querySelector("#searchingCity").value
-    showWhatCity(input)
-    
-    
-}document.querySelector("#submitInput").addEventListener("click",showCity)
-
-
-
+function showCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#searchingCity").value;
+  showWhatCity(input);
+}
+document.querySelector("#submitInput").addEventListener("click", showCity);
 
 //current weather
 
-function showMyPosition(){
-    navigator.geolocation.getCurrentPosition(position)
-        function position(position){
-            let lon = position.coords.longitude
-            let lat = position.coords.latitude
-            let apiKey="0a438755c0214c93834115811221606"
-            let apiURL=`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`
-     
-            axios.get(apiURL).then(showApi) 
-        }
-        
+function showMyPosition() {
+  navigator.geolocation.getCurrentPosition(position);
+  function position(position) {
+    let lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+    let apiKey = "0a438755c0214c93834115811221606";
+    let apiURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`;
 
-}document.querySelector("#getCurentPosition").addEventListener("click",showMyPosition)
-
-
+    axios.get(apiURL).then(showApi);
+  }
+}
+document
+  .querySelector("#getCurentPosition")
+  .addEventListener("click", showMyPosition);
